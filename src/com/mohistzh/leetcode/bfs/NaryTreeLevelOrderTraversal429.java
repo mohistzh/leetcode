ff03e92e45eb6cb1df608a1f543259213c8020e2
Z <mohistzh@gmail.com>
2020-09-28 22:07:50 +0800

    NaryTreeLevelOrderTraversal: BFS Approach

---
+++ src/com/mohistzh/leetcode/bfs/NaryTreeLevelOrderTraversal429.java
@@ -0,0 +1,117 @@
+package com.mohistzh.leetcode.bfs;
+//Given an n-ary tree, return the level order traversal of its nodes' values.
+//
+// Nary-Tree input serialization is represented in their level order traversal, 
+//each group of children is separated by the null value (See examples). 
+//
+// 
+// Example 1: 
+//
+// 
+//
+// 
+//Input: root = [1,null,3,2,4,null,5,6]
+//Output: [[1],[3,2,4],[5,6]]
+// 
+//
+// Example 2: 
+//
+// 
+//
+// 
+//Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null
+//,12,null,13,null,null,14]
+//Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The height of the n-ary tree is less than or equal to 1000 
+// The total number of nodes is between [0, 10^4] 
+// 
+// Related Topics Tree Breadth-first Search 
+// 👍 688 👎 47
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class NaryTreeLevelOrderTraversal429 {
+
+    /**
+     * BFS Approach
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+                if (current.children != null) {
+                    level.add(current.val);
+                    for (Node node : current.children) {
+                        queue.add(node);
+                    }
+                }
+
+            }
+            result.add(level);
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
